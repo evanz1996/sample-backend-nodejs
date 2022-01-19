333cb2e31882bdc1a4d0a57a741d927fa3c70e0c
Vanessa Valenzuela <vanessavalenzuela08@gmail.com>
2022-01-19 19:47:08 +0800

    logout function
    logout function
    users data  real format

--- app/function/employee.js
+++ app/function/employee.js
@@ -1,5 +1,20 @@
 const fs = require('fs');
 
+function logoutUser(token) {
+  console.log('tokenfront', token);
+  var tokenLogoutAPI = {
+    refresh_token:
+      'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjo2LCJleHAiOjE2NDAwMjA1ODZ9.yGQsymwwDpvm9DAQpRq8ung2qQEHKEJHJvAEjBTIsgiTzf2UAtyOWNXXfehAUvEObOCjRrwUwjoqZvr325iLPOPnMOf0ye4UufkAYMAcehzaODRYsDqPwY3uXcJa8APRGawmbhZy4Aw0jZhlflc_gX63G35WHIL-MknxfPsFyBlc5xg6pe92f9vM-2NL59dvAzqKEfhzn7agoXilJMrnPva4iXCfloP9Q_Uz_NPv1egJAZFnIPk0BZePHk3UiahoiL_WxwS0jVSVcPIPfDYT1hNDtOdDRVmCnwH6YdBrOA7Q3uLyWu-t-jQ9BFzk-iwBG1z9LosPaBOWLUfAsHhMp5homzI8kIFXjAtjOMahg22jtO7fMK94O5dz5vn2OWKyh0Li8kwW7QpAG5JfRQCxXvcxyneygVRiVwVPOPw82k1Do8DXAAc1rYQqR-_wsN0PlJ_i17efXxa6M5xnKY0rWk7E_S1r9FEbqbzrHPGH4ejkCN_RmPTIRFZ5nc41Lmh8h4aelTd1JaNv1kCMMDBrIhuFXGCO4n4CHvlAMre1ZweL3DhWrE8qqwbh2Z4_n0_kSiR6n9xtVfqSg91oovkCKF8HTGngdmFya1qIdFJWKngQIo1ncPa1KI3_v2vj-pWgDfvethGDj2FnDxrLpPpo0ePkCtobz76EKVU9eZD3LZs',
+    // refresh_token: 'a',
+  };
+  console.log('api token', tokenLogoutAPI.refresh_token);
+  if (tokenLogoutAPI.refresh_token == token) {
+    return 'Success';
+  } else {
+    return 'Error';
+  }
+}
+
 function getEmployees() {
   const jobDto = fs.readFileSync('./app/data/main-data.json'); //use to test in index.js
   var jobListDto = JSON.parse(jobDto);
@@ -10,7 +25,7 @@ function getEmployees() {
 function validatePhoneNumber(phoneNumber) {
   const users = fs.readFileSync('./app/data/main-data.json'); //use to test in index.js
   var userList = JSON.parse(users);
-  let userFilter = userList.filter((i) => i.PhoneNumber == phoneNumber);
+  let userFilter = userList.filter((i) => i.user.phone_number == phoneNumber);
   if (userFilter[0] != null) {
     return true;
   }
@@ -18,11 +33,11 @@ function validatePhoneNumber(phoneNumber) {
   return false;
 }
 
-function validatePassCode(passCode, phoneNumber) {
+function validatePassCode(phoneNumber, passCode) {
   const users = fs.readFileSync('./app/data/main-data.json'); //use to test in index.js
   var userList = JSON.parse(users);
   let userFilter = userList.filter(
-    (i) => i.PassCode == passCode && i.PhoneNumber == phoneNumber
+    (i) => i.user.pass_code == passCode && i.user.phone_number == phoneNumber
   );
   if (userFilter[0] != null) {
     return userFilter[0];
@@ -45,14 +60,40 @@ function validatePassCode1(passCode, phoneNumber) {
 }
 
 function getSpecificEmployee(id) {
+  console.log(id);
   const jobDto = fs.readFileSync('./app/data/main-data.json'); //use to test in index.js
   var jobListDto = JSON.parse(jobDto);
-  let job = jobListDto.filter((i) => i.Id == id);
-  console.log(job);
+  let job = jobListDto.filter((i) => i.user.uuid == id);
+
+  console.log(jobListDto);
+
   return job[0];
 }
 
 function updateuser(isesave, Id) {
+  const jobDto = fs.readFileSync('./app/data/main-data.json'); //use to test in index.js
+  var jobListDto = JSON.parse(jobDto);
+  let newJobList = jobListDto.filter(function (obj) {
+    console.log('param', Id);
+    // console.log(Id);
+    console.log(obj.id);
+    return obj.user.uuid != Id;
+  });
+  console.log('new', newJobList);
+
+  let jobParse = [isesave];
+  let mergeData = [...newJobList, ...jobParse];
+  let finalData = JSON.stringify(mergeData);
+  fs.writeFile('./app/data/main-data.json', finalData, (err) => {
+    // Error checking
+    if (err) throw err;
+    console.log('Updated');
+  });
+
+  return 'Success';
+}
+
+function updateLoggedInUser(isesave, Id) {
   const jobDto = fs.readFileSync('./app/data/main-data.json'); //use to test in index.js
   var jobListDto = JSON.parse(jobDto);
   let newJobList = jobListDto.filter(function (obj) {
@@ -121,6 +162,8 @@ exports.getSpecificEmployee = getSpecificEmployee;
 exports.updateuser = updateuser;
 exports.postEmployee = postEmployee;
 exports.validatePassCode1 = validatePassCode1;
+exports.updateLoggedInUser = updateLoggedInUser;
+exports.logoutUser = logoutUser;
 
 // exports.getJob = getJob;
 // exports.postJob = postJob;
--- index.js
+++ index.js
@@ -8,22 +8,37 @@ app.use(cors());
 app.use(express.json());
 
 // Create Users
+app.post('/v1/buyers/logout', (req, res) => {
+  console.log('req body', req.body);
+  let result = employeeFunc.logoutUser(req.body.refresh_token);
+  console.log(result);
+  res.json(result);
+});
+
+// Create Users
 app.post('/api/syr/create', (req, res) => {
   console.log(req.body);
   let result = employeeFunc.postEmployee(req.body);
   console.log(result);
   res.json(result);
 });
-// Update Users
+// Update Loggin
 app.put('/api/syr/updateuser/:id', (req, res) => {
   console.log(req.body);
   let result = employeeFunc.updateuser(req.body, req.params.id);
   console.log(result);
   res.json(result);
 });
+// Update Users
+app.put('/api/syr/updateuser/:id', (req, res) => {
+  // console.log(req.body);
+  let result = employeeFunc.updateuser(req.body, req.params.id);
+  // console.log(result);
+  res.json(result);
+});
 app.get('/api/syr/fetchUser/:id', (req, res) => {
   let result = employeeFunc.getSpecificEmployee(req.params.id);
-  console.log(result);
+  // console.log(result);
   res.json(result);
 });
 
@@ -37,15 +52,15 @@ app.get('/api/syr', (req, res) => {
   res.json('true');
 });
 
-app.get('/api/syr/validatephone/:phonenum', (req, res) => {
+app.get('/v1/users/phone_login/:phonenum', (req, res) => {
   let result = employeeFunc.validatePhoneNumber(req.params.phonenum);
   res.json(result);
 });
 
-app.get('/api/syr/validatepasscode/:passcode/:phonenum', (req, res) => {
+app.get('/v1/users/passcode/:phonenum/:passcode/', (req, res) => {
   let result = employeeFunc.validatePassCode(
-    req.params.passcode,
-    req.params.phonenum
+    req.params.phonenum,
+    req.params.passcode
   );
   res.json(result);
 });
